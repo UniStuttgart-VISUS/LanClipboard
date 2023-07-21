8557960c1d06a82fe4b061576d5d6e66e12692ae
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2023-07-21 13:01:18 +0200

    Changed the API to binary-only

--- LanClipboard.psm1
+++ LanClipboard.psm1
@@ -5,8 +5,7 @@ function Invoke-SdtpRequest {
         [int] $Port,
         [string] $Path,
         [ValidateSet("GET", "PUT", "DELETE")] [string] $Method = "GET",
-        [byte[]] $Body,
-        [switch] $Raw
+        [byte[]] $Body
     )
 
     try {
@@ -16,38 +15,42 @@ function Invoke-SdtpRequest {
         $writer = New-Object System.IO.StreamWriter($stream)
         $writer.AutoFlush = $true
 
-        Write-Verbose "$Method $($Path)"
-        $writer.Write("$Method $($Path) SDTP/1.0`n`n")
+        # Special handling for DELETE: This is acutally a GET on the server, but
+        # we do not want to emit the result in this case. Therefore, make the
+        # DELETE a GET, but remember that it actually was a delete in the
+        # original $Method parameter.
+        $m = if ($Method -ieq "DELETE") { "GET" } else { $Method }
+        Write-Verbose "$m $($Path)"
+        $writer.Write("$m $($Path) SDTP/1.0`n`n")
 
         if ($Body) {
             Write-Verbose "Writing message body ..."
             $stream.Write($Body, 0, $Body.Length)
         }
 
-        if ($Method -ieq "GET") {
-            if ($Raw) {
-                Write-Verbose "Reading binary response ..."
-                $ms = New-Object System.IO.MemoryStream
-                $stream.CopyTo($ms)
-                $retval = $ms.ToArray();
-                $msg = [System.Text.Encoding]::UTF8.GetString($retval)
+        if ($m -ieq "GET") {
+            Write-Verbose "Reading response ..."
+            $ms = New-Object System.IO.MemoryStream
+            $stream.CopyTo($ms)
+            $retval = $ms.ToArray();
 
-                if ($msg -imatch "^SDTP-ERROR:.+") {
+            # If there were an error message, this would be ASCII, so convert it
+            # as such and check whether we want to emit an error. For
+            # performance reasons, convert only as much as we need to determine
+            # whether this is an error.
+            if ($retval.Length -gt 11) {
+                $msg = [System.Text.Encoding]::ASCII.GetString($retval, 0, 11)
+                if ($msg -ieq "SDTP-ERROR:") {
+                    $msg = [System.Text.Encoding]::ASCII.GetString($retval)
                     Write-Error $msg
-                } else {
-                    Write-Output $retval
+                    return
                 }
+            }
 
-            } else {
-                Write-Verbose "Reading text response ..."
-                $reader = New-Object System.IO.StreamReader($stream)
-                $retval = $reader.ReadToEnd()
-
-                if ($retval -imatch "^SDTP-ERROR:.+") {
-                    Write-Error $retval
-                } else {
-                    Write-Output $retval
-                }
+            # Only emit something if the original request was GET. If we had a
+            # delete and did not detect an error before, just emit nothing.
+            if ($Method -ieq "GET") {
+                Write-Output $retval
             }
         }
 
@@ -62,6 +65,21 @@ function Invoke-SdtpRequest {
 }
 
 
+# Helper function to interpret responses.
+function Convert-Response {
+    param(
+        [byte[]] $Data,
+        [System.Text.Encoding] $Encoding
+    )
+
+    if ($Encoding) {
+        Write-Output $Encoding.GetString($Data)
+    } else {
+        Write-Output $Data
+    }
+}
+
+
 <#
 .SYNOPSIS
 Gets data from the LAN clipboard.
@@ -72,19 +90,20 @@ allows for retrieving the history of the clipboard as well as specific versions
 stored on the server.
 
 .PARAMETER Clipboard
-The Clipboard parameter specifies the name of the clipboard to retrieve data
-from. This parameter defaults to "_".
+The Clipboard parameter specifies the name of one or more clipboards to retrieve
+data from. This parameter defaults to "_".
 
 .PARAMETER Version
 The Version parameter selects the version of the clipboard to retrieve. If not
 specified, the latest version is retrieved.
 
+.PARAMETER Encoding
+The Encoding parameter performs a string conversion of the incoming byte stream
+on demand. If this parameter is not set, the raw byte stream from the server
+will be emitted to the pipeline.
+
 .PARAMETER History
 The History switch retrieves the history information for the clipboard.
-
-.PARAMETER Raw
-The Raw switch instructs the cmdlet to perform a binary read on the of the
-output and returning a byte array.
 
 .PARAMETER Server
 The Server parameter specifies the host name or address of the server hosting
@@ -95,7 +114,7 @@ The Port parameter specifies the port on which the LAN clipboard is listening
 on. This parameter default to 80.
 
 .INPUTS
-This cmdlet does not accept any inputs from the pipeline.
+You can pipe the names of the clipboards to retrieve to the cmdlet.
 
 .OUTPUTS
 The value in the clipboard.
@@ -116,42 +135,45 @@ Get-LanClipboard -Clipboard data2023 -History
 Get-LanClipboard -Clipboard data_2023 -Version 5 -Server fextest.rus.uni-stuttgart.de
 
 .EXAMPLE
-Get-LanClipboard -Raw | Add-Content -Path clipboard.bin -Encoding Byte
+"data_2022", "data_2023" | Get-LanClipboard
+
+.EXAMPLE
+Get-LanClipboard data_2023 -Encoding ([System.Text.Encoding]::UTF8)
 #>
 function Get-LanClipboard {
     [CmdletBinding()]
     param(
-        [Parameter(ParameterSetName = "Get", Position = 0)]
-        [Parameter(ParameterSetName = "History", Position = 0)]
-        [string] $Clipboard = "_",
+        [Parameter(Position = 0, ValueFromPipeline)]
+        [string[]] $Clipboard = "_",
 
-        [Parameter(ParameterSetName = "Get", Position = 1)]
+        [Parameter(Position = 1)]
         [int] $Version,
 
-        [Parameter(Mandatory = $true, ParameterSetName = "History")]
+        [Parameter(Position = 2)]
+        [System.Text.Encoding] $Encoding,
+
         [switch] $History,
-
-        [Parameter(ParameterSetName = "Get")]
-        [switch] $Raw,
-
-        [Parameter(ParameterSetName = "Get")]
-        [Parameter(ParameterSetName = "History")]
         [string] $Server = "fex.rus.uni-stuttgart.de",
-
-        [Parameter(ParameterSetName = "Get")]
-        [Parameter(ParameterSetName = "History")]
         [int] $Port = 80
     )
 
-    if ($History) {
-        Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$($Clipboard):q"
+    begin { }
 
-    } elseif ($Version) {
-        Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$($Clipboard):$Version" -Raw:$Raw
+    process {
+        $Clipboard | ForEach-Object {
+            if ($History) {
+                Convert-Response -Data (Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$($_):q") -Encoding ([System.Text.Encoding]::ASCII)
 
-    } else {
-        Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$($Clipboard)" -Raw:$Raw
+            } elseif ($Version) {
+                Convert-Response -Data (Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$($_):$Version") -Encoding $Encoding
+
+            } else {
+                Convert-Response -Data (Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$($_)") -Encoding $Encoding
+            }
+        }
     }
+
+    end { }
 }
 
 
@@ -164,7 +186,7 @@ Removes a whole clipboard and all of its version by name or deletes individual
 versions from a specific clipboard.
 
 .PARAMETER Clipboard
-The Clipboard parameter specifies the name of the clipboard to delete.
+The Clipboard parameter specifies the name of one or more clipboards to delete.
 
 .PARAMETER Version
 The Version parameter restricts the data to be deleted on a specific version.
@@ -178,7 +200,7 @@ The Port parameter specifies the port on which the LAN clipboard is listening
 on. This parameter default to 80.
 
 .INPUTS
-This cmdlet does not accept any pipeline inputs.
+The cmdlet accepts the names of clipboards to remove as input.
 
 .OUTPUTS
 This cmdlet does not emit any outputs to the pipeline.
@@ -195,8 +217,8 @@ Remove-LanClipboard -Clipboard data -Version 1
 function Remove-LanClipboard {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory, Position = 0)]
-        [string] $Clipboard,
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+        [string[]] $Clipboard,
 
         [Parameter(Position = 1)]
         [int] $Version,
@@ -205,12 +227,21 @@ function Remove-LanClipboard {
         [int] $Port = 80
     )
 
-    if ($Version) {
-        Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$($Clipboard):$Version" -Method DELETE
-    } else {
-        Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$Clipboard" -Method DELETE
+    begin { }
+
+    process {
+        $Clipboard | ForEach-Object {
+            if ($Version) {
+                Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$($_):$Version:d" -Method DELETE
+            } else {
+                Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$($_):D" -Method DELETE
+            }
+        }
     }
+
+    end { }
 }
+
 
 <#
 .SYNOPSIS
@@ -223,17 +254,11 @@ create it. Pipeline will always be interpreted as text. Use the other parameter
 sets if other behaviour is desired.
 
 .PARAMETER Value
-The Value parameter specifies the data to be stored in the clipboard. Note
-that this value will be converted into its string representation.
-
-.PARAMETER Raw
-The Raw parameter specifies an array of binary data to be stored in the
-clipboard. No conversion will be performed on the bytes provided.
+The Value parameter specifies the data to be stored in the clipboard.
 
 .PARAMETER Path
-The Path parameter specifies the path to a file which of the content will be
-stored in the clipboard. The file will be read in binary mode and no conversion
-will be performed on its contents.
+The Path parameter specifies the path to a file that is to be transferred to
+the LAN clipboard.
 
 .PARAMETER Clipboard
 The Clipboard parameter specifies the name of the clipboard to retrieve data
@@ -259,48 +284,27 @@ The Value parameter is accepted via the pipeline.
 This cmdlet does not emit any outputs to the pipeline.
 
 .EXAMPLE
-Set-LanClipboard foo
+Set-LanClipboard foo -Encoding ([System.Text.Encoding]::UTF8)
 
 .EXAMPLE
-Set-LanClipboard bar data_2023
+,(gc -Encoding Byte data.bin) | Set-LanClipboard foo -Clipboard data_2023
 
 .EXAMPLE
-Set-LanClipboard -Value bar -Clipboard data_2023
-
-.EXAMPLE
-Set-LanClipboard -Value bar -Clipboard data_2023 -Server fextest.rus.uni-stuttgart.de
-
-.EXAMPLE
-$env:COMPUTERNAME | Set-LanClipboard -Server fextest.rus.uni-stuttgart.de
-
-.EXAMPLE
-Set-LanClipboard -Value bar -Encoding ([System.Text.Encoding]::UTF32)
-
-.EXAMPLE
-Set-LanClipboard -Path image.png
-
-.EXAMPLE
-Set-LanClipboard -Raw (Get-Content -Path image.png -Encoding Byte)
+Set-LanClipboard -Path data.bin -Clipboard data_2023
 #>
 function Set-LanClipboard {
     [CmdletBinding()]
     param(
-        [Parameter(ParameterSetName = "String", Position = 0, ValueFromPipeline = $true)]
-        [string] $Value,
+        [Parameter(ParameterSetName="Value", Mandatory, Position = 0, ValueFromPipeline)]
+        $Value,
 
-        [Parameter(ParameterSetName = "Binary", Position = 0)]
-        [byte[]] $Raw,
-
-        [Parameter(ParameterSetName = "File", Position = 0)]
+        [Parameter(ParameterSetName="Path", Mandatory)]
         [string] $Path,
 
-        [Parameter(ParameterSetName = "String", Position = 1)]
-        [Parameter(ParameterSetName = "Binary", Position = 1)]
-        [Parameter(ParameterSetName = "File", Position = 1)]
+        [Parameter(Position = 1)]
         [string] $Clipboard = "_",
 
-        [Parameter(ParameterSetName = "String")]
-        [System.Text.Encoding] $Encoding = [System.Text.Encoding]::UTF8,
+        [System.Text.Encoding] $Encoding,
 
         [string] $Server = "fex.rus.uni-stuttgart.de",
         [int] $Port = 80
@@ -310,17 +314,13 @@ function Set-LanClipboard {
 
     process {
         if ($Value) {
-            $Value | ForEach-Object {
-                Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$Clipboard" -Method PUT -Body $Encoding.GetBytes("$_")
-            }
-            
-        } elseif ($Data) {
-            Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$Clipboard" -Method PUT -Body $Data
-
+            $data = if ($Encoding) { $Encoding.GetBytes("$Value") } else { [byte[]] $Value }
         } elseif ($Path) {
-            $data = Get-Content -Path $Path -Encoding Byte
-            Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$Clipboard" -Method PUT -Body $data
+            Write-Verbose "Reading raw data from `"$Path`" ..."
+            $data = (Get-Content -Encoding byte $Path)
         }
+
+        Invoke-SdtpRequest -Server $Server -Port $Port -Path "/lcb/$Clipboard" -Method PUT -Body $data
     }
 
     end { }
@@ -336,6 +336,7 @@ Export-ModuleMember -Function Set-LanClipboard
 New-Alias -Name glcb -Value Get-LanClipboard -Scope Global
 New-Alias -Name lcbget -Value Get-LanClipboard -Scope Global
 New-Alias -Name rlcb -Value Remove-LanClipboard -Scope Global
+New-Alias -Name rmlcb -Value Remove-LanClipboard -Scope Global
 New-Alias -Name lcbrm -Value Remove-LanClipboard -Scope Global
 New-Alias -Name slcb -Value Set-LanClipboard -Scope Global
 New-Alias -Name lcbput -Value Set-LanClipboard -Scope Global
